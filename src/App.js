fa4ce92f7352f13b852fabe31016551341c0e755
ajg7 <ajgebara@gmail.com>
2020-08-18 14:45:19 -0500

    created state for team members and form values

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [teamMembers, setTeamMembers] = useState([]);
+  const [formValues, setFormValues] = useState("")
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
